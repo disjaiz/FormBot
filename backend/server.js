849075000ad540bb25bc1feaafc1ca1bb0aa7ae2
Siddhi Jaiswal <145190948+disjaiz@users.noreply.github.com>
2025-01-03 00:03:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const routerUser = require('./Routes/User.js');
 const cors = require('cors');
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
