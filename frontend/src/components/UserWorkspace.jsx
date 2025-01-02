fc032baa1bffcd3aa4619d3aba55782240a27fe6
Siddhi Jaiswal <145190948+disjaiz@users.noreply.github.com>
2025-01-03 00:45:51 +0530

    Update UserWorkspace.jsx

--- frontend/src/components/UserWorkspace.jsx
+++ frontend/src/components/UserWorkspace.jsx
@@ -36,7 +36,7 @@ export default function UserWorkspace() {
   const handleDeleteFolder = async (folderId) => {
     console.log('folderid:',  folderId)
     try {
-      const response = await fetch("${url}/workspace/deleteFolder", {
+      const response = await fetch(`${url}/workspace/deleteFolder`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -70,7 +70,7 @@ export default function UserWorkspace() {
   const handleDeleteForm = async (formId) => {
     console.log('formid:',  formId)
     try {
-      const response = await fetch("${url}/workspace/deleteForm", {
+      const response = await fetch(`${url}/workspace/deleteForm`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -131,7 +131,7 @@ export default function UserWorkspace() {
 useEffect(() => {
   const fetchDefaultWorkspace = async () => {
     try {
-      const response = await fetch('${url}/workspace/find', {
+      const response = await fetch(`${url}/workspace/find`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
@@ -179,7 +179,7 @@ if (ownerWorkspace) {
 
   const handleAddFolder = async (folderName) => {
     try {
-      const response = await fetch('${url}/workspace/addFolder', {
+      const response = await fetch(`${url}/workspace/addFolder`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -247,7 +247,7 @@ const handleSelectChange = async (selectedWorkspaceId) => {
 
   const handleLogout = async () => {
     try {
-      const response = await fetch('${url}/user/logout', {
+      const response = await fetch(`${url}/user/logout`, {
         method: 'POST',
         credentials: 'include', 
       });
